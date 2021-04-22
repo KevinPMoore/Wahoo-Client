584ecee13b39a4ae48ef0ee938d1259118ad82bf
Kevin Moore <moore.kevin.p@gmail.com>
2021-04-22 12:16:22 -0400

    set up dummy data in store and ReadMe todo list

--- src/utils/store.js
+++ src/utils/store.js
@@ -2,17 +2,61 @@
 
 const store = {
     user: {
-        userName: '',
-
+        id: 1,
+        userName: 'TestUser',
+        password: 'Secret123',
+        questions: [ 1 ],
+        answers: [ ],
+        points: 0,
+        averagePoints: 0
     },
     userList: [
-
+        {
+            id: 1,
+            userName: 'TestUser',
+            password: 'Secret123',
+            questions: [ 1 ],
+            answers: [ ],
+            points: 0,
+            averagePoints: 0
+        },
+        {
+            id: 2,
+            userName: 'AnswerMaster',
+            password: 'DoesNotM4tter',
+            questions: [ ],
+            answers: [ ],
+            totalPoints: 0,
+            averagePoints: 0
+        },
+        {
+            id: 3,
+            userName: 'R34llyC0nfus3d',
+            password: 'H4lpM30ut',
+            questions: [ 2, 3, 4 ],
+            answers: [ ],
+            points: 0,
+            averagePoints: 0
+        }
     ],
     questions: [
-
+        {
+            id: 1,
+            user_id: 1,
+            title: '',
+            moreInfo: '',
+            category: '',
+            answers: [ ]
+        }
     ],
     answers: [
-        
+        {
+            id: 1,
+            user_id: 3,
+            content: '',
+            best: false,
+            points: 0,
+        }
     ]
 };
 
